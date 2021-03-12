0f92027f46657a37a4d43b0adc3ce38424e13dc0
astridmydnaa <80499173+astridmydnaa@users.noreply.github.com>
2021-03-12 09:41:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,23 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
+# Mengambil base image dari node
+FROM node:8
+ 
+# Membuat direktori aplikasi pada container
+WORKDIR /app
+ 
+# Memindahkan dependency aplikasi ke working directory
+COPY package*.json ./
+ 
+# Menginstall dependency
+RUN npm install
+ 
+# Memindahkan seluruh berkas ke dalam direktori container
+COPY . .
+ 
+# Mengatur container untuk membuka dan menggunakan port 8000
+EXPOSE 8000
+ 
+# Perintah untuk menjalankan aplikasi
+CMD [ "npm", "start" ]
